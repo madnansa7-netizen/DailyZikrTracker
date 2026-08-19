28025e5877700f9820da89ca0242f7afd59db04d
madnansa7-netizen <madnansa7@gmail.com>
2026-08-19 08:21:09 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,15 @@ android {
         versionName = "1.0"
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     buildFeatures {
         compose = true
     }
@@ -24,6 +33,7 @@ android {
 
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2025.04.01"))
+
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
